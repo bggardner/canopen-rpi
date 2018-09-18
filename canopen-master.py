437ca462932ba4bd44f652367259a654d6bb0151
Brent Gardner <brent@ebrent.net>
2018-09-18 14:08:20 +0000

    Bugfixes

--- canopen-master.py
+++ canopen-master.py
@@ -361,12 +361,6 @@ while True:
                     ),
                 })
 
-                try:
-                    node.boot()
-                    print("Rebooted")
-                except NameError:
-                    print("First boot")
-
                 with CANopen.Node(active_bus, node_id, canopen_od, run_indicator=runled0, err_indicator=errled0) as node:
                     while True:
                         pass # Replace with application code and interact with Object Dictionary (node.od)
